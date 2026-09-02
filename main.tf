7cbfe4b16a4fdc09481377b9461dbf7527934687
spyustrrrrr <sulthan.yustr@gmail.com>
2026-09-02 15:32:40 +0700

    fix: mengisi file aplikasi, main, dan playbook

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Mengunduh image Nginx (simulasi sistem operasi server)
+resource "docker_image" "nginx_server" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# ... (blok provider dan docker_image biarkan sama) ...
+
+resource "docker_container" "web_server" {
+  image = docker_image.nginx_server.image_id
+  name  = "server-terraform-variabel"
+  ports {
+    internal = 80
+    external = var.port_eksternal_nginx
+  }
+}
